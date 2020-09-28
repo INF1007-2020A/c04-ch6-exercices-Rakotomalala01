b3a05ab1f84bfae91bc8b26f22876002783ce65d
Rakotomalala01 <johary.rakotomalala@polymtl.ca>
2020-09-28 23:45:41 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -21,6 +21,8 @@ def anagrams(words: list = None) -> bool:
 def contains_doubles(items: list) -> bool:
     for i in items:
         if i == i-1
+        break
+    return cont
 
     return False
 
